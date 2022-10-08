3b07266bef87e9811a03549458d7e157b62b9232
sara pulido <sara01rizo@gmail.com>
2022-10-08 16:19:23 +0200

    Chall: Task list it is working CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,41 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = set_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(strong_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = set_task
+  end
+
+  def update
+    @task = set_task
+    @task.update(strong_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = set_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
   end
 
-
+  def strong_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get 'tasks', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: :new
   get '/tasks/:id', to: 'tasks#show', as: :task
 
+  post '/tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_restaurant
+
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
